b41479dbbeaf5b56a690c369e481b53c602e4cb0
sergey <s.orlov@findevelop.ru>
2016-12-26 17:45:15 +0700

    init

--- src/database/migrations/migrations.php
+++ src/database/migrations/migrations.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFootprintsTable extends Migration
+class CreateUtmRecorderTables extends Migration
 {
     /**
      * Run the migrations.
